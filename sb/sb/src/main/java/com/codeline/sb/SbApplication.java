9e549fc47937d72563e390ab940b01be40fc11e4
Ghaida02a <ggmskaj@gmail.com>
2025-11-18 16:13:36 +0400

    Created CRUD Apis for Course Object

---
+++ sb/sb/src/main/java/com/codeline/sb/SbApplication.java
@@ -0,0 +1,15 @@
+package com.codeline.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class SbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbApplication.class, args);
+
+	}
+}
